b3cf32cefa1fb90bc9276401d7aebf60f4492fc2
GitHub Action <action@github.com>
2023-02-04 13:00:44 +0000

    Template cleanup

--- src/main/kotlin/com/github/jriera/webstormplugintest/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/jriera/webstormplugintest/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.jriera.webstormplugintest.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.jriera.webstormplugintest.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
